a0b771c07b1e299efcaef53787e96bc538045252
Nelson Filipe Costa <nelsoncosta.jr17@gmail.com>
2022-06-29 16:25:20 -0400

    added new classic_ml.py file

---
+++ classic_ml.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import sklearn
+import sklearn.svm
+import sklearn.tree
+import sklearn.ensemble
+import sklearn.model_selection
+import sklearn.metrics
+import scipy
+import scipy.stats
